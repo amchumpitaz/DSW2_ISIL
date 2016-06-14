58c6ae94a7c3e771a5bd1205f4212de02d88a3c8
AMCHUMPI <aldichumpi@gmail.com>
2016-06-13 22:53:46 -0500

    Mantenimiento de Citas

---
+++ Proyecto/Librerias/Librerias.Isil.DentalSuite.Entidades/beCita.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librerias.Isil.DentalSuite.Entidades
+{
+    public class beCita
+    {
+        public string codigoCita { get; set; }
+        public DateTime fechaCita { get; set; }
+        public string codigoEspecialidad { get; set; }
+        public string codigoPaciente { get; set; }
+        public  
+    }
+}
---
+++ Proyecto/Librerias/Librerias.Isil.DentalSuite.Entidades/beHorarioOdontologo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librerias.Isil.DentalSuite.Entidades
+{
+    public class beHorarioOdontologo
+    {
+        public string CodigoHorarioOdontologo { get; set; }
+        public string CodigoOdontologo { get; set; }
+        public string CodigoHorario { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public String Estado { get; set; }
+
+        
+    }
+}
